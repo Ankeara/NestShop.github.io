c6beeb842e70a9900b796da59595edf9b2cafe38
ankeara <121712057+Ankeara@users.noreply.github.com>
2024-01-25 23:57:17 +0700

    Add files via upload

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,1 +1,25 @@
+// toggle category Browse
+const showCategory = document.querySelector(".drop-cate");
+const openCate = document.getElementById("openCate");
 
+openCate.addEventListener("click", function () {
+  showCategory.classList.toggle("add-cate");
+});
+// toggle category Browse
+
+// open aside
+const showAside = document.querySelector(".aside-tablet");
+const closeAside = document.getElementById("close-side");
+const openAside = document.querySelector(".openSide");
+const closeFixed = document.querySelector(".close-fixed-tablet");
+
+openAside.addEventListener("click", function () {
+  showAside.classList.add("showSideTablet");
+});
+closeAside.addEventListener("click", function () {
+  showAside.classList.remove("showSideTablet");
+});
+closeFixed.addEventListener("click", function () {
+  showAside.classList.remove("showSideTablet");
+});
+// open aside
